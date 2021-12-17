c118b12506b22e8e330b80e2cd09a31fa5dc86e7
huseyinbattal <huseyinbattal42@gmail.com>
2021-12-17 17:29:02 +0300

    firs commit

--- deneme/script.js
+++ deneme/script.js
@@ -1,60 +1,60 @@
-// function sayHello(ad=" guest"){
+function sayHello(ad=" guest"){
 
-//     const greeting="Hello"+ad;
-//     return greeting;
-// }
+    const greeting="Hello"+ad;
+    return greeting;
+}
 
-// console.log(sayHello(" john wayne"));
-// console.log(sayHello());
+console.log(sayHello(" john wayne"));
+console.log(sayHello());
 
-// const sayHello = function (name) {
-//     const greeting = "Hello" + name;
-//     return greeting;
+const sayHello = function (name) {
+    const greeting = "Hello" + name;
+    return greeting;
 
-// }
+}
 
-// console.log(sayHello(" Huseyin"));
+console.log(sayHello(" Huseyin"));
 
-// (
-//     function () {
-//         console.log("Welcome Hüseyin");
+(
+    function () {
+        console.log("Welcome Hüseyin");
 
-//     }
+    }
 
-//         ()
-// );
+        ()
+);
 
-// (function(userName){
-//     console.log("welcome",userName);
+(function(userName){
+    console.log("welcome",userName);
 
-// }("Hüseyin"));
+}("Hüseyin"));
 
-// function a(b){
-//     document.write(`merhaba ${b}`);
-// }
+function a(b){
+    document.write(`merhaba ${b}`);
+}
 
-// a("Hüseyin");
+a("Hüseyin");
 
-// let myAge=prompt("Yaşınızı giriniz.")
-// console.log(myAge);
+let myAge=prompt("Yaşınızı giriniz.")
+console.log(myAge);
 
-// function writeMyAge(params){
+function writeMyAge(params){
 
-//     document.write(`Senin yaşın ${myAge}`)
-// }
+    document.write(`Senin yaşın ${myAge}`)
+}
 
-//writeMyAge(myAge);
+writeMyAge(myAge);
 
-// let firstName = prompt("İsminizi giriniz.");
-// let firstAge = prompt("Yaşınızı giriniz.");
+let firstName = prompt("İsminizi giriniz.");
+let firstAge = prompt("Yaşınızı giriniz.");
 
-// function example(value1, value2) {
-//     let firstYears = 2 * 10.5;
-//     let lastYears = (value2 - 2) * 4;
-//     let totalYears = firstYears + lastYears;
+function example(value1, value2) {
+    let firstYears = 2 * 10.5;
+    let lastYears = (value2 - 2) * 4;
+    let totalYears = firstYears + lastYears;
 
-//     document.write(`<h1>${value1} köpek yaşın ${totalYears}</h1>`);
-// }
+    document.write(`<h1>${value1} köpek yaşın ${totalYears}</h1>`);
+}
 
-// example(firstName,firstAge);
+example(firstName,firstAge);
 
